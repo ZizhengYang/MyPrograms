009a07a8479d746cd5b044f453e1de8fba590271
Zizheng Yang <32321397+procccp@users.noreply.github.com>
2019-01-11 10:42:08 -0600

    Add files via upload

--- Algorithms/Sorting/InsertionSort.java
+++ Algorithms/Sorting/InsertionSort.java
@@ -4,16 +4,6 @@ import java.util.Arrays;
 
 public class InsertionSort {
 	
-	public static void main(String[] args) {
-		
-		int arr[] = {5, 2, 4, 6, 1, 3};
-		insertionSortSmallToLarge(arr);
-		System.out.println(Arrays.toString(arr));
-		insertionSortLargeToSmall(arr);
-		System.out.println(Arrays.toString(arr));
-		
-	}
-	
 	public static void insertionSortSmallToLarge(int[] arr){
 		
 		for(int i = 1; i < arr.length; i++){
