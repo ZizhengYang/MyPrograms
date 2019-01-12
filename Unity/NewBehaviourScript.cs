ce03a9b6905fbc05e2f22f9ff98d05e8d43ca81e
Zizheng Yang <32321397+procccp@users.noreply.github.com>
2019-01-12 11:46:03 -0600

    Add files via upload

---
+++ Unity/NewBehaviourScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    public static int moveNum;
+    /*
+     * 0: position
+     * 1: rotation
+     * 2: scale
+     */
+    public int[] moveType = new int[moveNum];
+    public double[,] creditcalPoints = new double[moveNum, 10];
+
+    public static int objNum;
+    public GameObject[] obj = new GameObject[objNum];
+    private double[,] position = new double[objNum, 3];
+    private double[,] rotation = new double[objNum, 3];
+    private double[,] scale = new double[objNum, 3];
+
+    // Use this for initialization
+    void Start () {
+        /*
+         * Initializing...
+         */
+		for(int i = 0; i < objNum; i++)
+        {
+            position[i, 0] = obj[i].transform.position.x;
+            position[i, 1] = obj[i].transform.position.y;
+            position[i, 2] = obj[i].transform.position.z;
+            rotation[i, 0] = obj[i].transform.rotation.x;
+            rotation[i, 1] = obj[i].transform.rotation.y;
+            rotation[i, 2] = obj[i].transform.rotation.z;
+            scale[i, 0] = obj[i].transform.localScale.x;
+            scale[i, 1] = obj[i].transform.localScale.y;
+            scale[i, 2] = obj[i].transform.localScale.z;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+}
---
+++ Unity/NewSurfaceShader.shader
@@ -0,0 +1,35 @@
+﻿Shader "Custom/DoubleSurface" {
+	Properties{
+		_Color("Main Color", Color) = (1,1,1,1)//Tint Color
+		_MainTex("Base (RGB)", 2D) = "white" {} //背面纹理
+		_MainTex_2("Base (RGB)", 2D) = "white" {} //正面纹理
+	}
+
+		SubShader{
+		Tags{ "RenderType" = "Opaque" }    //设置渲染类型 Opaque不透明
+		LOD 100
+
+		Pass{
+		Cull Front       //关闭正面渲染
+		Lighting Off
+		SetTexture[_MainTex]{ combine texture }
+		SetTexture[_MainTex]
+			{
+			ConstantColor[_Color]
+			Combine Previous * Constant
+			}
+		}
+
+		Pass
+		{
+		Cull Back      //关闭背面渲染
+		Lighting Off
+		SetTexture[_MainTex_2]{ combine texture }
+		SetTexture[_MainTex_2]
+			{
+			ConstantColor[_Color]
+			Combine Previous * Constant
+			}
+		}
+	}
+}
