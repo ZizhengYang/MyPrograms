35b56b24d7fa8d2f939208346215211d2692da73
Zizheng Yang <32321397+procccp@users.noreply.github.com>
2019-01-14 23:52:24 -0600

    Add files via upload

--- Algorithms/Sorting/InsertionSorts/InsertionSort/InsertionSort.java
+++ Algorithms/Sorting/InsertionSorts/InsertionSort/InsertionSort.java
@@ -1,11 +1,33 @@
-package Sort.InsertionSort;
+package Sort.InsertionSort.InsertionSort;
 
+/**
+ * 
+ * <table>
+ * 		<tr>
+ * 			<th>Worst-case performance: &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</th>
+ * 			<th>n^2</th>
+ * 		</tr>
+ * 		<tr>
+ * 			<th>Best-case performance: &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</th>
+ * 			<th>n</th>
+ * 		</tr>
+ * 		<tr>
+ * 			<th>Average performance: &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</th>
+ * 			<th>n^2</th>
+ * 		</tr>
+ * 		<tr>
+ * 			<th>Worst-case space complexity: &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</th>
+ * 			<th>n</th>
+ * 		</tr>
+ * </table>
+ *
+ */
 public class InsertionSort {
-	
-	public static void insertionSortSmallToLarge(int[] arr){
-		
+
+	public static void insertionSort_SmallToLarge(int[] arr){
+
 		for(int i = 1; i < arr.length; i++){
-		
+
 			int pivot = arr[i];
 			int j = i - 1;
 			while(j >= 0 && arr[j] > pivot){
@@ -13,18 +35,18 @@ public class InsertionSort {
 				j--;
 			}
 			arr[j+1] = pivot;
-			
+
 		}
-		
+
 	}
 	
-	public static void insertionSortLargeToSmall(int[] arr){
+	public static void insertionSort_LargeToSmall(int[] arr){
 		
 		for(int i = 1; i < arr.length; i++){
 		
 			int pivot = arr[i];
 			int j = i - 1;
-			
+
 			while(j >= 0 && arr[j] < pivot){
 				arr[j+1] = arr[j];
 				j--;
