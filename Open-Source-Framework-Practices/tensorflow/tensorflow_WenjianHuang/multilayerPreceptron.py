63b043dcb1a2e281f0a79524599426b22b53d073
Zizheng Yang <32321397+procccp@users.noreply.github.com>
2019-01-19 10:21:13 -0600

    Add files via upload

--- Open-Source-Framework-Practices/tensorflow/tensorflow_WenjianHuang/multilayerPreceptron.py
+++ Open-Source-Framework-Practices/tensorflow/tensorflow_WenjianHuang/multilayerPreceptron.py
@@ -13,3 +13,31 @@ w1 = tf.Variable(tf.truncated_normal([INPUT, HIDDEN], stddev = 0.1))
 b1 = tf.Variable(tf.zeros([HIDDEN]))
 w2 = tf.Variable(tf.truncated_normal([HIDDEN, OUTPUT], stddev = 0.1))
 b2 = tf.Variable(tf.zeros([OUTPUT]))
+
+x = tf.placeholder(tf.float32, [None, INPUT])
+keep_prob = tf.placeholder(tf.float32)          # a probability of dropping nodes
+
+hidden1 = tf.nn.relu(tf.matmul(x, w1) + b1)
+hidden1_drop = tf.nn.dropout(hidden1, keep_prob)
+y = tf.nn.softmax(tf.matmul(hidden1_drop, w2) + b2)
+
+y_ = tf.placeholder(tf.float32, [None, 10])
+
+loss = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), axis=[1]))
+train_step = tf.train.AdagradOptimizer(0.3).minimize(loss)
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+init = tf.global_variables_initializer()
+sess.run(init)
+
+for i in range(3000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    losses, step = sess.run([loss, train_step], feed_dict={x:batch_xs, y_:batch_ys, keep_prob: 0.75})
+    acc = sess.run(accuracy, feed_dict={x:mnist.test.images, y_:mnist.test.labels, keep_prob: 1.0})
+    print("The current training step is: ", end="")
+    print(step, end="")
+    print("; losses is: ", end="")
+    print(losses, end="")
+    print("; accuracy is: ", end="")
+    print(acc)
