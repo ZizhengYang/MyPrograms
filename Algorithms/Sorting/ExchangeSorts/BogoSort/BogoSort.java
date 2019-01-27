9fe4801f1d28b0f1778cf8a74bb30f2de8741422
Zizheng Yang <32321397+procccp@users.noreply.github.com>
2019-01-26 20:55:35 -0600

    Add files via upload

--- Algorithms/Sorting/ExchangeSorts/BogoSort/BogoSort.java
+++ Algorithms/Sorting/ExchangeSorts/BogoSort/BogoSort.java
@@ -1,1 +1,29 @@
+package Sort.ExchangeSorts.BogoSort;
 
+import java.util.Random;
+
+public class BogoSort {
+	
+	static Random random = new Random();
+	
+	public static void bogoSort(int[] n) {
+	    while(!inOrder(n))shuffle(n);
+	}
+
+	public static void shuffle(int[] n) {
+	    for (int i = 0; i < n.length; i++) {
+	        int swapPosition = random.nextInt(i + 1);
+	        int temp = n[i];
+	        n[i] = n[swapPosition];
+	        n[swapPosition] = temp;
+	    }
+	}
+
+	public static boolean inOrder(int[] n) {
+	    for (int i = 0; i < n.length-1; i++) {
+	        if (n[i] > n[i+1]) return false;
+	    }
+	    return true;
+	}
+
+}
