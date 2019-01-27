24176886d68fd9c0398f55dc264afbad477049aa
Zizheng Yang <32321397+procccp@users.noreply.github.com>
2019-01-26 20:25:56 -0600

    Add files via upload

--- Algorithms/Sorting/ExchangeSorts/GnomeSort/GnomeSort.java
+++ Algorithms/Sorting/ExchangeSorts/GnomeSort/GnomeSort.java
@@ -1,1 +1,35 @@
+package Sort.ExchangeSorts.GnomeSort;
 
+public class GnomeSort {
+	
+	public static void gnomeSort_SmallToLarge(int[] arr) {
+		int position = 0;
+		while(position < arr.length) {
+			if(position == 0 || arr[position] > arr[position-1]) {
+				position++;
+			}
+			else {
+				int temp = arr[position];
+				arr[position] = arr[position-1];
+				arr[position-1] = temp;
+				position--;
+			}
+		}
+	}
+	
+	public static void gnomeSort_LargeToSmall(int[] arr) {
+		int position = 0;
+		while(position < arr.length) {
+			if(position == 0 || arr[position] < arr[position-1]) {
+				position++;
+			}
+			else {
+				int temp = arr[position];
+				arr[position] = arr[position-1];
+				arr[position-1] = temp;
+				position--;
+			}
+		}
+	}
+
+}
