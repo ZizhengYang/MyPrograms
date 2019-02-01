2892c350ad02ccf3a6db097548e7607029b87cf6
Zizheng Yang <32321397+procccp@users.noreply.github.com>
2019-01-31 21:23:34 -0600

    Update html_parser.py

--- Web-Crawlers/original-python-edition/html_parser.py
+++ Web-Crawlers/original-python-edition/html_parser.py
@@ -13,9 +13,7 @@ class HTMLParser(object):
         soup = BeautifulSoup(html_cont, 'html.parser', from_encoding='utf-8')
         new_urls = self._get_new_urls(new_url, soup)
         new_data = self._get_new_data(new_url, soup)
-        print("parse is successfully called")
-        # print(new_urls)
-        # print(new_data)
+        # print("parse is successfully called")
         return new_urls, new_data
 
     def _get_new_data(self, new_url, soup):
@@ -42,5 +40,4 @@ class HTMLParser(object):
             new_full_url = urllib.parse.urljoin(new_url, url)
             new_urls.add(new_full_url)
         print("_get_new_urls is successfully called")
-        #print(new_urls)
         return new_urls
